2a19ffebac947345eb8e6274fc9a49a302ea2b4b
Gaurab <gkc@vols.utk.edu>
2018-07-11 16:23:43 -0400

    Uploads after compressing to zip
    client is able to send just files  or they cn send the directory.

--- portal/client.py
+++ portal/client.py
@@ -1,15 +1,53 @@
 import requests
 import sys
+import os
+import zipfile
 url = "https://pace.ornl.gov/dev1/upload"
 t=1
-if (len(sys.argv)<2):
-	print('Usage: python client.py filename.ext')
+
+def createOutputZip(zfname, filelist):
+	zf = zipfile.ZipFile("%s.zip" % (zfname), "w", zipfile.ZIP_DEFLATED)
+	for f in filelist:
+		arcname = zfname + "/" + f
+		print "Adding file: %s to archive: %s.zip" % (arcname, zfname)
+		zf.write(f,arcname)
+	zf.close()
+	return
+
+def zipSubdir(zfname, path):
+	zf = zipfile.ZipFile("%s.zip" % (zfname), "w", zipfile.ZIP_DEFLATED)
+	abs_src = os.path.abspath(path)
+	for root,dirs,files in os.walk(path):
+		for file in files:
+			absname = os.path.abspath(os.path.join(root, file))
+			arcname = zfname + "/" + absname[len(abs_src) + 1:]
+			print "Adding file: %s to archive: %s.zip" % (arcname, zfname)
+			zf.write(absname,arcname)
+	zf.close()
+	return
+
+print(len(sys.argv))
+if (len(sys.argv)<3):
+	print('Usage: python client.py "OPTION" "Files/Dir"')
+	print('OPTION = 1, for file uploads')
+	print('OPTION = 2, for directory uploads')
 else:
-	for i in range(len(sys.argv)-1):	
-		fin = open(sys.argv[i+1], 'rb')	
-		files = {'file': fin}
-		try:
-			r = requests.post(url, files=files)
-			print(r.text)
-		finally:
-			fin.close()
+	zfname='uploadzip'
+	opt = sys.argv[1]
+	if opt == '1':	
+		FileList = sys.argv[2: ]
+		print (FileList)
+		createOutputZip(zfname,FileList)	
+	elif opt == '2':
+		path=sys.argv[2]
+		zipSubdir(zfname,path)
+	else:
+		sys.exit("Invalid upload option, option are (1 or 2)")
+
+	fin = open('uploadzip.zip', 'rb')	
+	files = {'file': fin}
+	try:
+		r = requests.post(url, files=files)
+		print(r.text)
+	finally:
+		fin.close()
