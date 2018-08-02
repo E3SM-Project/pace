7437f2bda32f3b2fad4d2f77b886af865147666d
Sarat Sreepathi <sarat@ornl.gov>
2018-08-02 18:23:03 -0400

    Use Flask SQLAlchemy for db session
    Increase file upload limit too.

--- portal/pace/__init__.py
+++ portal/pace/__init__.py
@@ -2,6 +2,8 @@
 
 from flask import Flask
 from flask import render_template
+from pace_common import *
+from flask_sqlalchemy import SQLAlchemy
 
 # Import the fixer
 from werkzeug.contrib.fixers import ProxyFix
@@ -10,15 +12,18 @@ from werkzeug.contrib.fixers import ProxyFix
 from flask import request,redirect,url_for
 from werkzeug.utils import secure_filename
 
-UPLOAD_FOLDER = '/pace/prod/portal/uploads'
-
 app = Flask(__name__)
 # Use the fixer
 app.wsgi_app = ProxyFix(app.wsgi_app)
 
+dbConn, dbEngine, dburl = connectDatabase()
+app.config['SQLALCHEMY_DATABASE_URI'] = dburl;
+db = SQLAlchemy(app)
+
+UPLOAD_FOLDER = '/pace/prod/portal/uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-# Limit payload to 32 MB
-app.config['MAX_CONTENT_LENGTH'] = 128 * 1024 * 1024 
+# Limit payload to 1024 MB
+app.config['MAX_CONTENT_LENGTH'] = 1024 * 1024 * 1024 
 app.secret_key = 'p\xcb\xd8\x81z\xa5)D\x14(\x8dJ\nvjdb\x82\x9a\x8dH\rg='
 
 if __name__ == "__main__":
