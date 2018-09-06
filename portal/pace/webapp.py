25939d861413e67b87d4b04dd3751e44bb9efc25
Gaurab <gkc@vols.utk.edu>
2018-09-06 17:32:17 -0400

    Error handling in process

--- portal/pace/webapp.py
+++ portal/pace/webapp.py
@@ -43,8 +43,8 @@ def upload_file():
 			filename = secure_filename(file.filename)
 			file.save(os.path.join(UPLOAD_FOLDER, filename))
 			#os.system("/opt/venv/pace/bin/python /pace/prod/portal/pace/parse.py")
-			parse.parseData()
-			return('File Upload and Store in Database Success')
+			return(parse.parseData())
+			#return('File Upload and Store in Database Success')
 		else:
 			return ('Error Uploading file, Try again')
 	return render_template('upload.html')
