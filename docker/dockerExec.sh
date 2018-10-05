1b75b24ef1e22161e11ba2bee8ecb24e02e872ff
Zachary Mitchell <31785316+zachMitchell@users.noreply.github.com>
2018-10-05 12:50:58 -0400

    Moved docker related items to it's own folder (except Dockerfile)

--- docker/dockerExec.sh
+++ docker/dockerExec.sh
@@ -2,5 +2,5 @@ if [ $PACE_DEV = 1 ];
     then service ssh start;
     export FLASK_ENV=development;
     fi
-python ./portal/dockerInit.py&
+python portal/dockerInit.py&
 while [ 1 = 1 ]; do sleep 360;done
