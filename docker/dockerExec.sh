d7d41d69d217567865b794ea2a91cb6c3ce1a17d
zachMitchell <31785316+zachMitchell@users.noreply.github.com>
2018-10-22 12:52:23 -0400

    Changed everything to absolute paths in docker bash scripts

--- docker/dockerExec.sh
+++ docker/dockerExec.sh
@@ -2,21 +2,18 @@
 #This is a loop to keep pace containers running regardless of a crash. It will NOT restart flask.
 #It also creates a .pacerc file if one doesn't exist.
 # set -x
+dockerpath=/pace/docker
 if [ ! -f docker/rc/.pacerc ];
     then if (($#)); then
-        if [ $1 = "-file" ]; then 
-            echo -e "$2" >> /pace/docker/rc/.pacerc;
-        else
-            argStr=""
-            while (($#)); do
-                argStr="$argStr $1";
-                shift
-            done
-            python docker/newRc.py $argStr >> docker/rc/.pacerc;
-        fi
-        else python docker/newRc.py $PACE_RC_DEFAULTS >> docker/rc/.pacerc;
+        argStr=""
+        while (($#)); do
+            argStr="$argStr $1";
+            shift
+        done
+        python "$dockerpath"/newRc.py $argStr >> "$dockerpath"/rc/.pacerc;
+        else python "$dockerpath"/newRc.py $PACE_RC_DEFAULTS >> "$dockerpath"/rc/.pacerc;
     fi
-    chmod 600 docker/rc/.pacerc
+    chmod 600 "$dockerpath"/rc/.pacerc
 fi
 if [ $PACE_DEV ];
     then 
@@ -24,5 +21,5 @@ if [ $PACE_DEV ];
     export FLASK_ENV=development;
     fi
 # set +x
-python portal/dockerInit.py&
+python /pace/portal/dockerInit.py&
 while [ 1 = 1 ]; do sleep 360;done
