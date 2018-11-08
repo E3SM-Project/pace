020bf273372f9d81be99ec41878e1a2783a8baba
zachMitchell <31785316+zachMitchell@users.noreply.github.com>
2018-11-08 14:57:09 -0500

    newRc.py was updated to use the ConfigParser library

--- docker/newRc.py
+++ docker/newRc.py
@@ -2,7 +2,7 @@
 #Purpose: Create a new pacerc file. This either generates one, or grabs a "file" from the arguments.
 #This is designed to be used in docker where you can't send files through standard input, but it can also be used standalone.
 import sys
-
+import ConfigParser
 #The values in the rc file. the first index in each array is an alternate name, and the second is the name of the value.
 #These can be changed out for any other values one might want
 rcValues = {
@@ -10,17 +10,16 @@ rcValues = {
     "MINIO":["minio_access_key","minio_secret_key","minio_url"]
 }
 
-usrRcVals = {
-    "PACE":{},
-    "MINIO":{},
-}
+usrRcVals = ConfigParser.RawConfigParser()
+usrRcVals.add_section("PACE")
+usrRcVals.add_section("MINIO")
 
 #the rc values in string format:
 rcValStr = ""
 for key in rcValues.keys():
     rcValStr+="=="+key+"==\n"
     for element in rcValues[key]:
-        rcValStr+= element[0]+" | "+element[1]+"\n"
+        rcValStr+= element+"\n"
 
 usage = "Usage: "+sys.argv[0]+" arg1 arg1 [etc...] OR "+sys.argv[0]+" var1=value1 var2 = value2 etc=etc"+"""
 
@@ -31,9 +30,9 @@ The result is printed to stdout, so to save this config, the user would do the f
 
 List of variables:
 
-Shortcut | variable name
 """+rcValStr
 
+printResults = False
 equalPoints = 0 #This stores who many equal signs were detected
 if len(sys.argv) > 1 and sys.argv[1] in ["-h","--help"]:
     print(usage)
@@ -45,6 +44,7 @@ else:
             equalPoints+=1
 
 if equalPoints > 0:
+    printResults = True
     #Check to see if everything either matches a keyword, aor a shortcut word:
     #Sorry... This is a lot of for loops >_<
     for i in range(len(sys.argv)):
@@ -53,23 +53,22 @@ if equalPoints > 0:
                 for element in rcValues[category]:
                     targetStr = sys.argv[i].split("=")
                     if targetStr[0] == element:
-                        usrRcVals[category][element] = targetStr[1]
+                        usrRcVals.set(category,element, targetStr[1])
                         break
-    #Generate the file: (If the variable the user specified didn't exist, it won't be accounted for in the rc file)
-    for key in usrRcVals.keys():
-        print("["+key+"]")
-        for element in usrRcVals[key].keys():
-            print(element+" = "+usrRcVals[key][element])
 elif equalPoints == 0 and len(sys.argv) > 1:
-    #Print settings until we run out of arguments/settings
+    printResults = True
+    #Go through each argument and add them to the config until we run out.
     currArgIndex = 1
     for key in rcValues.keys():
-        print("["+key+"]")
         for element in rcValues[key]:
             if not currArgIndex == len(sys.argv):
-                print(element+" = "+sys.argv[currArgIndex])
+                usrRcVals.set(key,element,sys.argv[currArgIndex])
                 currArgIndex+=1
             else:
                 break
+#Print everything to stdout:
+if printResults:
+    with sys.stdout as console:
+        usrRcVals.write(console)
 else:
     print(usage)
