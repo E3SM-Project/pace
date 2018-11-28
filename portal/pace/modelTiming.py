6de5ab3ad46e14a6e7bc3f61e63e00e0800284a2
zachMitchell <31785316+zachMitchell@users.noreply.github.com>
2018-11-28 09:55:25 -0500

    Finished the core of modelTiming generalization!
    Documentation was created to help with creating new configs
    
    example files are available to demonstrate how custom configurations work

--- portal/pace/modelTiming.py
+++ portal/pace/modelTiming.py
@@ -2,13 +2,24 @@
 #Purpose: parse files that come from GPTL (https://jmrosinski.github.io/GPTL/) to create a programatic representation of the output.
 import types,json,os.path
 from os import listdir
-valueList=[
-    ["on","called","recurse","wallClock","max","min","utrOverhead"],
-    ["processes","threads","count","walltotal","wallmax","wallmax_proc","wallmax_thrd","wallmin","wallmin_proc","wallmin_thrd"]
-]
 
 #This is a list of configurations that tell the parser how to read specific GPTL files
-parserConfigs = {}
+parserConfigs = {
+"e3sm":[{
+        "names":["On","Called","Wallclock","max","min","UTR Overhead"],
+        "altNames":["on","called","recurse","wallClock","max","min","utrOverhead"],
+        "startMarker":["Stats for thread",2],
+        "fileIdentifiers":["Stats for thread"],
+        "rootParent":2
+    },
+    {
+        "names":["process","threads","count","walltotal","wallmax","wallmin"],
+        "altNames":["processes","threads","count","walltotal","wallmax","wallmax_proc","wallmax_thrd","wallmin","wallmin_proc","wallmin_thrd"],
+        "startMarker":["name",1],
+        "fileIdentifiers":["GLOBAL STATISTICS"],
+        "rootParent":0
+    }]
+}
 
 class timeNode(object):
     #Without this, a reeealy weird scoping problem happens; not sure why.
@@ -61,40 +72,59 @@ def loadConfig(files,appendLocation = None):
                 appendJson(open(element))
             elif os.path.isdir(element):
                 for filePath in listdir(element):
-                    print(element+"/"+filePath)
+                    # print(element+"/"+filePath)
                     if isJsonFile(element+"/"+filePath):
                         appendJson(open(element+"/"+filePath))
     return targetConfig
 
-#def detectMtFile(fileObj,config = parserConfigs):
-"""This returns the matching configuration of this file, along with the proper indexes to start getting the file's lines"""
+def detectMtFile(fileObj,configList = parserConfigs):
+    #This returns the matching configuration of this file, along with the proper indexes to start getting the file's lines
+    lineCount = 0
+    threadIndexes=[]
+    currLine = ""
+    targetConfig = None
+    #There appears to be multiple threads in some files, let's figure out where they are, then re-read the file:
+    currLine = fileObj.readline()
+    while not currLine == "":
+        lineCount+=1
+        #Scan every line to find everything we should know about the file:
+        if targetConfig == None:
+            for key in configList.keys():
+                for config in configList[key]:
+                    #Check to see if any of the file Identifiers showed up in this line:
+                    for fileID in config["fileIdentifiers"]:
+                        if fileID in currLine:
+                            targetConfig = config
+                            break
+                    #Next check to see if all the names from the config appear in the line:
+                    score = 0
+                    for name in config["names"]:
+                        if name in currLine:
+                            # print(name)
+                            score+=1
+                    if score == len(config["names"]):
+                        targetConfig = config
+                        break
+        if not targetConfig == None:
+            #Look for threadIndexes:
+            if targetConfig["startMarker"][0] in currLine:
+                threadIndexes.append(lineCount+targetConfig["startMarker"][1])
+        currLine = fileObj.readline()
+    #Reset the file & read from the new thread indexes
+    fileObj.seek(0,0)
+    return threadIndexes,targetConfig
     
-def getData(src):
+def getData(src,configList = parserConfigs):
     #Check if src is a string, otherwise attempt to read from a file object:
     sourceFile=None
-    isStat=False #if this is a model_timing_*_stats file, this will be flicked on.
     if type(src) == types.StringType:
         sourceFile = open(src,"r")
     else:
         sourceFile = src
     lineCount = 0
     resultLines=[]
-    threadIndexes=[]
-    currLine = ""
-    #There appears to be multiple threads in some files, let's figure out where they are, then re-read the file:
-    currLine = sourceFile.readline()
-    while not currLine == "":
-        lineCount+=1
-        if "Stats for thread" in currLine:
-            threadIndexes.append(lineCount+2)
-        elif "GLOBAL STATISTICS" in currLine:
-            isStat=True #we found a statistics file instead of a regular file.
-        if isStat and "name" in currLine:
-            threadIndexes.append(lineCount+1)
-        currLine = sourceFile.readline()
-    #Reset the file & read from the new thread indexes
-    sourceFile.seek(0,0)
-    lineCount=0
+
+    threadIndexes,fileConfig = detectMtFile(sourceFile,configList)
 
     for line in threadIndexes:
         resultLines.append([])
@@ -107,13 +137,13 @@ def getData(src):
                     break
                 #this became a little meta, so here's a reference in order to continue indexing:
                 thread = resultLines[len(resultLines)-1]
-                if countSpaces(currLine) == 2 or isStat or firstItr:
+                if countSpaces(currLine) == fileConfig["rootParent"] or firstItr:
                     thread.append([])
                     firstItr = False
                 thread[len(thread)-1].append(str(currLine) )
 
     sourceFile.close()
-    return resultLines
+    return resultLines,fileConfig
 
 #In theory, this should get used quite a bit, despite being simple...
 def countSpaces(strInput):
@@ -124,10 +154,7 @@ def countSpaces(strInput):
     return count
 
 #Recursive function that looks at one line, and checks to see if that line has children. (which in turn re-runs the function)
-def parseNode(lineInput,currLine=0,parent=None):
-    vlIndex=0
-    if "(" in lineInput[currLine]:
-        vlIndex=1
+def parseNode(lineInput,config,currLine=0,parent=None):
     resultNode = timeNode()
     if parent:
         resultNode.parent = parent
@@ -163,13 +190,13 @@ def parseNode(lineInput,currLine=0,parent=None):
         word = wordCheck.strip("()\n")
         if word not in ["","\n"]:
             if word not in ["-","y"]:
-                resultNode.values[valueList[vlIndex][valueCount]] = float(word.strip(')\n')) #The logic gate above fails to catch parenthesis with newlines, so this is here XP
+                resultNode.values[config["altNames"][valueCount]] = float(word.strip(')\n')) #The logic gate above fails to catch parenthesis with newlines, so this is here XP
                 valueCount+=1
             else:
                 tf = False
                 if word == 'y':
                     tf=True
-                resultNode.values[valueList[vlIndex][valueCount]] = tf
+                resultNode.values[config["altNames"][valueCount]] = tf
                 valueCount+=1
     #Try to find children until the indentation in the file no longer matches a child
     parentSpaceCount = countSpaces(lineInput[currLine])
@@ -177,7 +204,7 @@ def parseNode(lineInput,currLine=0,parent=None):
     if currLine < (len(lineInput)-1):
         while countSpaces(lineInput[childrenIndex]) > parentSpaceCount:
             if countSpaces(lineInput[childrenIndex]) == parentSpaceCount+2:
-                childNode = parseNode(lineInput,childrenIndex,resultNode)
+                childNode = parseNode(lineInput,config,childrenIndex,resultNode)
                 resultNode.children.append(childNode)
             childrenIndex+=1
             if childrenIndex > len(lineInput)-1:
@@ -186,18 +213,18 @@ def parseNode(lineInput,currLine=0,parent=None):
 
 #When reading threads from a file, the returned list goes down to three dimensions! This "meta function" is to help with those headaches.
 #This function assumes you're inserting thread(s) from getData()
-def parseThread(thread):
+def parseThread(thread,config):
     if len(thread) == 0:
         return []
     elif type(thread[0][0]) == types.StringType:
         resultNodes = []
         for nodes in thread:
-            resultNodes.append(parseNode(nodes))
+            resultNodes.append(parseNode(nodes,config))
         return resultNodes
     elif type(thread[0][0]) == types.ListType:
         resultThreads=[]
         for element in thread:
-            resultThreads.append(parseThread(element))
+            resultThreads.append(parseThread(element,config))
         return resultThreads
 
 #Return a node with the first instance of the requested name; this function only works with parsed nodes.
@@ -247,11 +274,11 @@ def toJson(nodeIn,useBrackets=True):
 
 #The cherry on top: All functions above are called, and a complete JSON output is returned from all the layers of obscurity.
 #In case you want pre-json output, returnLayer lets you stop the compilation process at any moment and return the current output. Besides JSON, the end result will always be stored in a list
-def parse(fileIn,returnLayer=2):
-    nodeLines = getData(fileIn)
+def parse(fileIn,configList = parserConfigs,returnLayer=2):
+    nodeLines,config = getData(fileIn,configList)
     if returnLayer == 0:
         return nodeLines
-    resultThreads = parseThread(nodeLines)
+    resultThreads = parseThread(nodeLines,config)
     if returnLayer == 1:
         return resultThreads
     return toJson(resultThreads)
