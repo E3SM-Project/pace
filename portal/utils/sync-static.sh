117dc5d70db8bc1bf474b6acbec8461971069ad9
Sarat Sreepathi <sarat@ornl.gov>
2018-11-30 17:21:57 -0500

    pace-upload typo fix and sync file

--- portal/utils/sync-static.sh
+++ portal/utils/sync-static.sh
@@ -1,2 +1,2 @@
 sudo rsync -avz /pace/prod/portal/pace/static/ /pace/assets/static
-cp client/pace-upload /pace/assets/static/tools/pace-upload
+cp ../../client/pace-upload /pace/assets/static/tools/pace-upload
