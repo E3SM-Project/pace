182bb10ac69e02b03b193a53c5c1db02dca9c784
zachMitchell <31785316+zachMitchell@users.noreply.github.com>
2018-12-03 09:49:11 -0500

    Flamegraphs can be accessed through the homepage
    Fixed margin issues with the flamegraph (Resizing is still strange)

--- portal/pace/webapp.py
+++ portal/pace/webapp.py
@@ -268,7 +268,7 @@ def summaryQuery(expID,rank):
         resultNodes = json.dumps([newJson])
     return  '{{"obj":{0},"meta":{{"expid":"{1}","rank":"{2}","compset":"{3}","res":"{4}"}} }}'.format(resultNodes,expID,rank,compset,res)
 
-@app.route("/flamegraph/<expid>/<rank>")
+@app.route("/flamegraph/<expid>/<rank>/")
 def flameGraph(expid,rank):
     return render_template("flameGraph.html",expid=expid,rank=rank)
 
