36232659a46231937c39f260d00794b62ef96605
Sarat Sreepathi <sarat@ornl.gov>
2018-12-07 17:25:56 -0500

    sudo is changing permissions on logs dir and causing issues with upload.

--- portal/utils/sync-static.sh
+++ portal/utils/sync-static.sh
@@ -1,2 +1,2 @@
-sudo rsync -avz /pace/prod/portal/pace/static/ /pace/assets/static
+rsync -avz /pace/prod/portal/pace/static/ /pace/assets/static
 cp /pace/prod/client/pace-upload /pace/assets/static/tools/pace-upload
