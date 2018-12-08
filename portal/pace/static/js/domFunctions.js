00fc3798909d9f2b441dafa9108dba603814e634
Zachary Mitchell <31785316+zachMitchell@users.noreply.github.com>
2018-12-08 17:18:27 -0500

    The info bubble was moved to mtGeneralTools.js for use in the atmosphere chart

--- portal/pace/static/js/domFunctions.js
+++ portal/pace/static/js/domFunctions.js
@@ -1,6 +1,5 @@
 //Author: Zachary Mitchell
 //Purpose: This file holds all of DOM functions in modelTiming.html (It got rather large XP)
-var isChrome = /Chrome/.test(navigator.appVersion);
 var triggerResize;
 var dlLock = true; //Locks dataList in place. If it slides back, so will dataInfo
 var dlShow = true;
@@ -423,39 +422,7 @@ dataInfo.onwheel = function(evt){
     }
 }
 
-/*There's a bug (either in google chrome or in this metaOpenClose) that stretches the metatable after closing it.
-I'm guessing this is a chrome bug, since the debugger doesn't show values being changed in any way. The below function fixes this issue.*/
-function metaContainerChromeFix(){
-    if(isChrome && metaTable.style.display == "none"){
-        metaInfoContainer.style.display="none";
-        setTimeout(()=>metaInfoContainer.style.display="inline",10);
-    }
-}
-
-//Open and close the meta-info box
-function metaOpenClose(openClose=false,metaArray){
-    if(metaArray){
-        let outStr=""
-        if(metaArray.length > 1){
-            outStr = `<span onclick='$(metaTable).slideToggle(200,metaContainerChromeFix)' style='cursor:pointer'>Click to view more details about these experiments.</span>
-            <div id='metaTable' style="display:none;text-align:left;margin-left:25%"><table><thead><tr><th>Compset</th><th>Res</th><th>Expid</th></tr></thead><tbody>`;
-            metaArray.forEach(exp=>{
-                outStr+="<tr><td>"+exp.compset+"</td><td>"+exp.res+"</td><td><a href='"+detectRootUrl()+"exp-details/"+exp.name+"'>"+exp.name+"</a></td></tr></div>";
-            });
-            outStr+="</tbody></table>"
-        }
-        else{
-            outStr = "Compset: <a href='"+detectRootUrl()+"advsearch/compset:"+metaArray[0].compset+"'>"+metaArray[0].compset+"</a> Res: <a href='"+detectRootUrl()+"advsearch/res:"+metaArray[0].res+"'>"+metaArray[0].res+"</a> (<a href='"+detectRootUrl()+"exp-details/"+metaArray[0].name+"'>Details</a>)";
-        }
-        $(metaInfoTxt.parentElement).slideUp(200);
-        setTimeout(()=>metaInfoTxt.innerHTML = outStr,metaInfoTxt.innerHTML == ""?0:200);
-        $(metaInfoTxt.parentElement).slideDown(200);
-    }
-    if(openClose)
-        $(metaInfoTxt.parentElement).slideDown(200);
-    else
-        $(metaInfoTxt.parentElement).slideUp(200);
-}
+//The meta info-box has been moved to mtGeneralTools.js
 
 //The interface for the color selection:
 var colorSelect = {
--- portal/pace/static/js/mtGeneralTools.js
+++ portal/pace/static/js/mtGeneralTools.js
@@ -1,6 +1,8 @@
 //Author: Zachary Mitchell
 //Purpose: This file is here in the event where more than one tool could come in handy for the modelTiming json format
 
+var isChrome = /Chrome/.test(navigator.appVersion);
+
 //This will be where we look for a specific node. (Making the structured time-node into something linear at the same time)
 function addressTable(vals=undefined,jsonArray=false,srcExp,thread = -1){
     let newAddressTable = [];
@@ -31,4 +33,39 @@ function addressTable(vals=undefined,jsonArray=false,srcExp,thread = -1){
         }
     }
     return newAddressTable
+}
+
+/*There's a bug (either in google chrome or in this metaOpenClose) that stretches the metatable after closing it.
+I'm guessing this is a chrome bug, since the debugger doesn't show values being changed in any way. The below function fixes this issue.*/
+function metaContainerChromeFix(){
+    if(isChrome && metaTable.style.display == "none"){
+        metaInfoContainer.style.display="none";
+        setTimeout(()=>metaInfoContainer.style.display="inline",10);
+    }
+}
+
+//Open and close the meta-info box
+function metaOpenClose(openClose=false,metaArray){
+    if(metaArray){
+        let outStr=""
+        if(metaArray.length > 1){
+            outStr = `<span onclick='$(metaTable).slideToggle(200,metaContainerChromeFix)' style='cursor:pointer'>Click to view more details about these experiments.</span>
+            <div id='metaTable' style="display:none;text-align:left;margin-left:25%"><table><thead><tr><th>Compset</th><th>Res</th><th>Expid</th></tr></thead><tbody>`;
+            metaArray.forEach(exp=>{
+                outStr+="<tr><td>"+exp.compset+"</td><td>"+exp.res+"</td><td><a href='"+detectRootUrl()+"exp-details/"+exp.name+"'>"+exp.name+"</a></td></tr></div>";
+            });
+            outStr+="</tbody></table>"
+        }
+        else{
+            outStr = "Compset: <a href='"+detectRootUrl()+"advsearch/compset:"+metaArray[0].compset+"'>"+metaArray[0].compset+"</a> Res: <a href='"+detectRootUrl()+"advsearch/res:"+metaArray[0].res+"'>"+metaArray[0].res+"</a> (<a href='"+detectRootUrl()+"exp-details/"+metaArray[0].name+"'>Details</a>)";
+        }
+        $(metaInfoContainer).slideUp(200);
+        metaInfoTxt = metaInfoContainer.children[0];
+        setTimeout(()=>metaInfoTxt.innerHTML = outStr,metaInfoTxt.innerHTML == ""?0:200);
+        $(metaInfoContainer).slideDown(200);
+    }
+    if(openClose)
+        $(metaInfoContainer).slideDown(200);
+    else
+        $(metaInfoContainer).slideUp(200);
 }
