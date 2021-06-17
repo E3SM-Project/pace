58d67bac5b6b29af26cf30287b2d3286e0fc67f3
Gaurab KC <gkc@vols.utk.edu>
2021-06-17 15:19:20 -0400

    test Modeltiming

--- portal/pace/test_modelTiming_parser.py
+++ portal/pace/test_modelTiming_parser.py
@@ -1,13 +1,16 @@
 import modelTiming as mt
 
-print "new timing stats"
+myjson = mt.parse('/Users/4g5/Downloads/exp-blazg-71436/timing.43235257.210608-222102/model_timing.0000')
+print(myjson)
+exit()
+print("new timing stats")
 myjson = mt.parse('modelTimingTest/gptl_timing_stats_new.txt')
 # print myjson
-print "old timing stats"
+print("old timing stats")
 myjson = mt.parse('modelTimingTest/gptl_timing_stats_old.txt')
 # print myjson
-print "rank timing"
+print("rank timing")
 myjson = mt.parse('modelTimingTest/model_timing.0000')
-print myjson
+print(myjson)
 
 # print myjson
