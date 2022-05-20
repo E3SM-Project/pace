4fa842fa3048114d64eb7987dee5d1684d6c7ca3
Gaurab KC <gkc@vols.utk.edu>
2022-05-20 10:39:10 -0400

    initial replay sh parse

---
+++ portal/pace/e3sm/e3smParser/parseReplaysh.py
@@ -0,0 +1,12 @@
+
+import gzip
+
+def load_replayshFile(replayshfile):
+    data = None
+
+    try:
+        with gzip.open(replayshfile, 'rt') as f:
+            data = f.read()
+        return data
+    except:
+        print("Error encountered while parsing replay.sh file : %s" %replayshfile)    
