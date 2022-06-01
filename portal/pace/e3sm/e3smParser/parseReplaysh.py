8fbabe3a5ea2f93f3843868e131a372fd955a857
Gaurab KC <gkc@vols.utk.edu>
2022-06-01 15:52:01 -0400

    skips if .sh file is empty

--- portal/pace/e3sm/e3smParser/parseReplaysh.py
+++ portal/pace/e3sm/e3smParser/parseReplaysh.py
@@ -7,6 +7,9 @@ def load_replayshFile(replayshfile):
     try:
         with gzip.open(replayshfile, 'rt') as f:
             data = f.read()
+        if not data:
+            return None
         return data
     except:
         print("Error encountered while parsing replay.sh file : %s" %replayshfile)
+        return None
--- portal/pace/e3sm/e3smParser/parseRunE3SMsh.py
+++ portal/pace/e3sm/e3smParser/parseRunE3SMsh.py
@@ -1,5 +1,7 @@
 
+from fileinput import filename
 import gzip
+import io
 
 def load_rune3smshfile(rune3smshfile):
     data = None
@@ -7,6 +9,9 @@ def load_rune3smshfile(rune3smshfile):
     try:
         with gzip.open(rune3smshfile, 'rt') as f:
             data = f.read()
+        if not data:
+            return None
         return data
     except:
         print("Error encountered while parsing run_e3sm.sh file : %s" %rune3smshfile)
+        return None
