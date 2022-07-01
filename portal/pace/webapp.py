b2c15aa2f6fd6c7e7585fa13a345d7a026d6e073
Gaurab KC <gkc@vols.utk.edu>
2022-07-01 13:18:13 -0400

    other time to be positive

--- portal/pace/webapp.py
+++ portal/pace/webapp.py
@@ -1116,7 +1116,7 @@ def atmos(expids):
                 return render_template("error.html")
             note = ""
             if actual_other_time < 0:
-                note = "* Note: For this experiment, the high-level component timer (e.g., ATM_RUN) is less than the cumulative sum of the selected sub-process timers. So, the cumulative sum is used for the stacked plot. This happens due to variance in MPI_wallmax timers across processors. It is typically harmless provided that it is a small percentage. The absolute difference in timer values is " + str(round(actual_other_time,2)) + " seconds and the percentage error is "+str(percentError)+"%."
+                note = "* Note: For this experiment, the high-level component timer (e.g., ATM_RUN) is less than the cumulative sum of the selected sub-process timers. So, the cumulative sum is used for the stacked plot. This happens due to variance in MPI_wallmax timers across processors. It is typically harmless provided that it is a small percentage. The absolute difference in timer values is " + str(round((-1)*actual_other_time,2)) + " seconds and the percentage error is "+str(percentError)+"%."
             return render_template("modelComponentProcess.html",expids = expid, jd = jsonData,note = note, model = 'ATM')
         else:
             UIData = {}
